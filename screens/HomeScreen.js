d3de6c99b869c2c7cd15a7d517cb3994ed7493f9
rashedInt32 <parvez08eee@gmail.com>
2019-12-08 23:28:46 +0600

    Initial setup with basic expo app with tab

---
+++ screens/HomeScreen.js
@@ -0,0 +1,40 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { MonoText } from '../components/StyledText';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  title: 'Some',
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
